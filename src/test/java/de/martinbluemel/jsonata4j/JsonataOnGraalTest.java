643d15a27eb8ccdda6a557018ec2b61ae3afeb3c
Martin Bluemel <martin.bluemel@web.de>
2022-09-03 13:05:01 +0200

    Improved error message for missing original sources from JSONata

--- src/test/java/de/martinbluemel/jsonata4j/JsonataOnGraalTest.java
+++ src/test/java/de/martinbluemel/jsonata4j/JsonataOnGraalTest.java
@@ -1,7 +1,7 @@
 package de.martinbluemel.jsonata4j;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 
@@ -11,17 +11,27 @@ class JsonataOnGraalTest {
 
 	@Test
 	void map() {
-		assertTrue(new File("src/main/js/jsonata.js").exists());
-		assertTrue(new File("src/main/js/parser.js").exists());
-		assertTrue(new File("src/main/js/signature.js").exists());
-		assertTrue(new File("src/main/js/functions.js").exists());
-		assertTrue(new File("src/main/js/utils.js").exists());
-		String mapped = new JsonataOnGraal().map(JsonataMapper.readFile(new File("src/test/resources/mapping01/in.json")),
+		assertJsonataSourceFile("jsonata");
+		assertJsonataSourceFile("parser");
+		assertJsonataSourceFile("signature");
+		assertJsonataSourceFile("datetime");
+		assertJsonataSourceFile("functions");
+		assertJsonataSourceFile("utils");
+		String mapped = new JsonataOnGraal().map(
+				JsonataMapper.readFile(new File("src/test/resources/mapping01/in.json")),
 				JsonataMapper.readFile(new File("src/test/resources/mapping01/mapping.jsonata")));
 		System.out.println("@@@ TEST @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		System.out.println(mapped);
 		System.out.println("@@@ TEST @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		// unfortunately we do yet not get any value back from main.js
 		assertEquals(null, mapped);
+	}
+
+	private void assertJsonataSourceFile(String sourceName) {
+		File jsonataSourceFile = new File("src/main/js/" + sourceName + ".js");
+		if (!jsonataSourceFile.exists()) {
+			fail("Missing JSONata Javascript source file: " + jsonataSourceFile.getAbsolutePath()
+					+ ", Please download from https://github.com/jsonata-js/jsonata");
+		}
 	}
 }
