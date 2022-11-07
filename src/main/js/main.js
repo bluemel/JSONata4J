610b91d23e2d78b39103648c919c027f4f505588
Peter Boy <kopa@gmx.com>
2022-11-07 21:03:21 +0000

    fix: the input value should be a json objet

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,13 +1,13 @@
-var jsonata = require("./jsonata");
+const jsonata = require("./jsonata");
 
-console.log('@@@ JS mapping @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-console.log(mapping);
-console.log('@@@ JS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-var expression = jsonata(mapping);
-console.log('@@@ expression = "' + expression + '"');
-console.log('@@@ JS input @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-console.log(input);
-console.log('@@@ JS output @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-var output = expression.evaluate(input);
-console.log(output);
-console.log('@@@ JS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+let json = JSON.parse(input)
+
+const output = jsonata(mapping).evaluate(json, {}, async (error, result) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+});
+output.then((value) => {
+    console.log(value)
+})
